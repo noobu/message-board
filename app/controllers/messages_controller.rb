6428cc3a67549a1f189af0d2652902f3a627a9c7
noobu <nobuhiro.nishi@gmail.com>
2016-03-27 04:32:35 +0000

    working1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,14 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+     redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      #メッセージが保存できなかったとき
+      @messages=Message.all
+      flash.now[:alert] = "メッセージに失敗しました。"
+      render 'index'
+    end
   end
 
   private
