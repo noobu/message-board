89615a85a5e5fb6d18779515f3a5f0cc345405b3
noobu <nobuhiro.nishi@gmail.com>
2016-03-28 11:55:17 +0000

    Gemfile update for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,8 @@ class MessagesController < ApplicationController
   end
 
   def create
+    binding.pry
+    
     @message = Message.new(message_params)
     if @message.save
      redirect_to root_path , notice: 'メッセージを保存しました'
